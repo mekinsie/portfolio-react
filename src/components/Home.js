6e46d3215dda41c2cad427039f0d4f62a0eb900f
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-05-12 23:47:57 -0700

    Move nav bar to top left

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,23 +20,23 @@ function Home(){
   return(
     <>
     <div>
+    <FadeIn transitionDuration='1500' delay="1000">
+      <ul>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">WORKS</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+    </FadeIn>
     <FadeIn transitionDuration='1500'>
       <NameHeader>Mekinsie</NameHeader>
-      </FadeIn>
-      <FadeIn transitionDuration='2000'>
-      <NameHeader>Callahan</NameHeader>
-      </FadeIn>
-      <FadeIn transitionDuration='2500'>
-      <Subheader>Software Developer</Subheader>
-      </FadeIn>
-    </div>
-    <FadeIn transitionDuration='1500' delay="1000">
-    <ul>
-      <li><a href="#">ABOUT</a></li>
-      <li><a href="#">WORKS</a></li>
-      <li><a href="#">CONTACT</a></li>
-    </ul>
     </FadeIn>
+    <FadeIn transitionDuration='2000'>
+      <NameHeader>Callahan</NameHeader>
+    </FadeIn>
+    <FadeIn transitionDuration='2500'>
+      <Subheader>Software Developer</Subheader>
+    </FadeIn>
+    </div>
     </>
   )
 }
