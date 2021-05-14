73dda8b46191f30cbbc07e2b92aeb4bf5b7b5480
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-05-13 18:54:43 -0700

    Add github repos and portfolio to works. Update README

--- README.md
+++ README.md
@@ -1,70 +1,34 @@
-# Getting Started with Create React App
+# _My Portfolio_
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+#### _A webpage that contains a collection of my current works_
 
-## Available Scripts
+#### By _**Mekinsie Callahan**_
 
-In the project directory, you can run:
+## Technologies Used
 
-### `npm start`
+* React
+* SCSS
+* JSX
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Description
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+_This portfolio contains an about me, background, list of current works, and my contact information. I created it to showcase my skills as a developer and designer._
 
-### `npm test`
+## Setup/Installation Requirements
+* _My portfolio can be viewed <a href="http://www.mekinsie.com/" target="_blank">here</a>._
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Known Bugs
 
-### `npm run build`
+* _No known bugs_
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## License
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+_GPL_
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Copyright (c) 2021 **_Mekinsie Callahan_**
 
-### `npm run eject`
+## Contact Information
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+Reach me: via <a href="https://www.linkedin.com/in/mekinsie/" target="_blank">Linkedin</a> or <a href="mailto:mekinsie.aja@gmail.com" target="_blank">email</a></li>.
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Checkout my <a href="https://github.com/mekinsie" target="_blank">github</a>.
--- src/components/Works.js
+++ src/components/Works.js
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 
 function Works(){
   const WorksDiv = styled.div`
-  height: 900px;
+  height: 1200px;
   margin-bottom: 100px;
   width: 600px;
   margin: auto;
-  // text-align: center;
   `;
 
   const WorksHeader = styled.h1`
@@ -28,29 +27,37 @@ function Works(){
   font-size: 17px;
   `;
 
+  const float = {
+    float: 'right'
+  }
+
   return(
     <>
     <WorksDiv id="works">
       <WorksHeader>Works</WorksHeader>
       <WorksLinks>
-        <a href="https://flash-chords.herokuapp.com/" target="_blank">Flash Chords</a>
-        <h5>Ruby on Rails | PostgreSQL | Heroku | Devise | SCSS.</h5>
-        <Description>Flash Chords is a web application where you can learn piano chords through the use of flash cards. Sign up to save and customize your own flashcard sets.</Description>
+        <a href="https://flash-chords.herokuapp.com/" target="_blank">FLASH CHORDS</a>
+        <h5>Ruby on Rails | PostgreSQL | Heroku | Devise | SCSS <a style={float} href="https://github.com/mekinsie/piano_pals" target="_blank">GitHub Repo</a></h5>
+        <Description>Flash Chords is a web application where users can learn piano chords through the use of flash cards. Users can sign up to save and customize their own flashcard sets. </Description>
 
-        <a href="https://www.memoryspace.net/" target="_blank">Memory Space</a>
-        <h5>React | Redux | Firebase </h5>
-        <Description>This application is designed as a community memory space. Users can add their favorite or insightful memories to share with other users.</Description>
+        <a href="https://www.memoryspace.net/" target="_blank">MEMORY SPACE</a>
+        <h5>React | Redux | Firebase <a style={float} href="https://github.com/mekinsie/memory-space" target="_blank">GitHub Repo</a> </h5>
+        <Description>This application is designed as a community memory space. Users can add their favorite or insightful memories to share with other users. It is currently populated with memories from anonymous users.</Description>
 
-        <a href="https://hitomis-sushi.herokuapp.com/" target="_blank">Hitomi's Sushi</a>
-        <h5>Ruby on Rails | PostgreSQL | Heroku | SCSS.</h5>
-        <Description>A sushi review website where users can login to add, edit, and delete their own reviews. Admin may add, edit and delete products while also deleting reviews. The database is seeded using the Faker gem.</Description>
+        <a href="https://www.mekinsie.com/" target="_blank">PORTFOLIO</a>
+        <h5>React | SCSS <a style={float} href="https://github.com/mekinsie/portfolio-react" target="_blank">GitHub Repo</a></h5>
+        <Description>This portolfio is a personal project to display my work. My design goal is for it to be minimalist and dynamic.</Description>
 
-        <a href="https://mekinsie.github.io/pig-dice/" target="_blank">Pig Dice</a>
-        <h5>Javascript | jQuery | HTML5 | CSS | Bootstrap</h5>
+        <a href="https://hitomis-sushi.herokuapp.com/" target="_blank">HITOMI'S SUSHI</a>
+        <h5>Ruby on Rails | PostgreSQL | Heroku | SCSS <a style={float} href="https://github.com/mekinsie/hitomis-sushi" target="_blank">GitHub Repo</a></h5>
+        <Description>A sushi review website where users can login to add, edit, and delete their own reviews. Admin may add, edit and delete products while also deleting reviews. The database is seeded using the Faker gem. </Description>
+
+        <a href="https://mekinsie.github.io/pig-dice/" target="_blank">PIG DICE</a>
+        <h5>Javascript | jQuery | HTML5 | CSS | Bootstrap <a style={float} href="https://github.com/mekinsie/pig-dice" target="_blank">GitHub Repo</a></h5>
         <Description>A game of Pig Dice. The purpose of this project was to practice using Object-Oriented JavaScript. Co-authored by <a href="https://github.com/KirbyPaint" target="_blank">KirbyPaint.</a></Description>
 
-        <a href="https://mekinsie.github.io/language-suggester/" target="_blank">Language Suggester</a>
-        <h5>Javascript | jQuery | HTML5 | CSS | Bootstrap</h5>
+        <a href="https://mekinsie.github.io/language-suggester/" target="_blank">LANGUAGE SUGGESTER</a>
+        <h5>Javascript | jQuery | HTML5 | CSS | Bootstrap <a style={float} href="https://github.com/mekinsie/language-suggester" target="_blank">GitHub Repo</a></h5>
         <Description>A lighthearted quiz that suggests the best coding language for you.</Description>
       </WorksLinks>
     </WorksDiv>
